1d2cf6d6a254dd10578e697406b0c6a164c6ccda
HitaloAguiar <hitalo.aguiar3767@gmail.com>
2024-09-13 19:47:35 -0300

    Alterações feitas após explicação sobre deffuzificação

--- logicaNebulosa/sistema_analise_risco_banco_fuzzy.py
+++ logicaNebulosa/sistema_analise_risco_banco_fuzzy.py
@@ -7,12 +7,12 @@ credito = ctrl.Antecedent(np.arange(0, 11, 1), 'credito')
 renda = ctrl.Antecedent(np.arange(0, 11, 1), 'renda')
 divida = ctrl.Antecedent(np.arange(0, 11, 1), 'divida')
 
-risco = ctrl.Consequent(np.arange(0, 11, 1), 'risco')
+risco = ctrl.Consequent(np.arange(0, 11, 1), 'risco', defuzzify_method='mom')
 
-credito['Ruim'] = fuzz.trimf(credito.universe, [0, 2, 4])
-credito['Regular'] = fuzz.trimf(credito.universe, [2, 4, 6])
-credito['Bom'] = fuzz.trimf(credito.universe, [4, 6, 8])
-credito['Excelente'] = fuzz.trimf(credito.universe, [6, 8, 10])
+credito['Ruim'] = fuzz.trapmf(credito.universe, [0, 0, 2, 3])
+credito['Regular'] = fuzz.trapmf(credito.universe, [2, 3, 6, 6])
+credito['Bom'] = fuzz.trapmf(credito.universe, [6, 7, 8, 9])
+credito['Excelente'] = fuzz.trapmf(credito.universe, [8, 9, 10, 10])
 
 renda['Baixa'] = fuzz.trimf(renda.universe, [0, 0, 5])
 renda['Média'] = fuzz.trimf(renda.universe, [0, 5, 10])
@@ -26,8 +26,8 @@ risco['Baixo'] = fuzz.trimf(risco.universe, [0, 0, 5])
 risco['Médio'] = fuzz.trimf(risco.universe, [0, 5, 10])
 risco['Alto'] = fuzz.trimf(risco.universe, [5, 10, 10])
 
-# credito.view()
-# plt.show()
+credito.view()
+plt.show()
 
 # renda.view()
 # plt.show()
@@ -41,7 +41,7 @@ risco['Alto'] = fuzz.trimf(risco.universe, [5, 10, 10])
 rule1 = ctrl.Rule(credito['Excelente'] & divida['Baixa'], risco['Baixo']) # SE o histórico de crédito é "Excelente" E a dívida atual é "Baixa", ENTÃO o risco é "Baixo. Independente da renda"
 rule2 = ctrl.Rule(credito['Ruim'] & divida['Alta'], risco['Alto']) # SE o histórico de crédito é "Ruim" E a dívida atual é "Alta", ENTÃO o risco é "Alto". Independente da renda
 rule3 = ctrl.Rule(credito['Bom'] & renda['Média'] & divida['Moderada'], risco['Médio']) # SE o histórico de crédito é "Bom" E a renda mensal é "Média" E a dívida atual é "Moderada", ENTÃO o risco é "Médio"
-rule4 = ctrl.Rule(credito['Regular'] & divida['Moderada'] & renda['Alta'] | renda['Média'], risco['Médio']) # SE o histórico de crédito é "Regular" E a dívida atual é "Moderada", o risco é "Moderado". A menos que a renda seja baixa
+rule4 = ctrl.Rule(credito['Regular'] & divida['Moderada'] & ~renda['Baixa'], risco['Médio']) # SE o histórico de crédito é "Regular" E a dívida atual é "Moderada", o risco é "Moderado". A menos que a renda seja baixa
 
 rule5 = ctrl.Rule(credito['Excelente'] & divida['Moderada'], risco['Baixo'])
 rule6 = ctrl.Rule(credito['Excelente'] & divida['Alta'] & renda['Média'] | renda['Alta'], risco['Baixo'])
@@ -64,9 +64,9 @@ banco_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7
 
 banco = ctrl.ControlSystemSimulation(banco_ctrl)
 
-banco.input['credito'] = 9.6
+banco.input['credito'] = 3
 banco.input['renda'] = 2
-banco.input['divida'] = 7.5
+banco.input['divida'] = 5
 
 banco.compute()
 
